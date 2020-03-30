5de29493b3cd4ff3acfdae83ff20c3edeba6b46b
tam876 <tammylee816@yahoo.com>
2020-03-30 18:45:11 -0400

    updated database url 2

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secretkey"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://yrymkwjahdzqbu:927e1d87bfb0d7bc021e4f2b5a6bf497f07faa76b5048fa02dab447813872232@ec2-54-197-48-79.compute-1.amazonaws.com:5432/dffrh5bmmdgr39"
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://lmwfeawvhmyfzj:fea5bc945a59365737e2243e38fe098d70094bea9605fd77d6d81ad0d32374a8@ec2-3-234-109-123.compute-1.amazonaws.com:5432/dd9bpvuq2b1qb1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER']="./app/static/uploads"
 
